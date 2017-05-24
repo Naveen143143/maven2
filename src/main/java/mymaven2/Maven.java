37b007d75895e552c771ff5e1d5162998e3842a0
naveen <naveen.maradana06@gmail.com>
2017-05-24 14:36:22 -0400

    maven commit

---
+++ src/main/java/mymaven2/Maven.java
@@ -0,0 +1,10 @@
+package mymaven2;
+
+public class Maven {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+
+	}
+
+}
---
+++ src/test/java/mymaven2/maventest.java
@@ -0,0 +1,14 @@
+package mymaven2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class maventest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
